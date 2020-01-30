ef10264b5f09985d5ce024223660d611a1d096b5
Léo Godard <leo.godard@epitech.eu>
2020-01-30 14:51:33 +0100

    Optimize init and fix epinorm

--- my_hashmap.h
+++ my_hashmap.h
@@ -16,62 +16,62 @@ typedef struct my_hashmap{
 #ifndef MY_HASHMAP
 #define MY_HASHMAP
 /**
-    * Get data from key in hashmap
-    * @return void *
+* Get data from key in hashmap
+* @return void *
 **/
 void *my_hashmap_get(my_hashmap *map, void *key);
 
 /**
-    * Get key from data in hashmap
-    * @return void *
+* Get key from data in hashmap
+* @return void *
 **/
 void *my_hashmap_get_from_data(my_hashmap *map, void *data);
 
 /**
-    * Does hashmap contains key
-    * @return 1 when true, 0 when false, -1 when map is NULL
+* Does hashmap contains key
+* @return 1 when true, 0 when false, -1 when map is NULL
 **/
 int my_hashmap_contains(my_hashmap *map, void const *key);
 
 /**
-    * @return 1 when true, 0 when false, -1 when map is NULL
+* @return 1 when true, 0 when false, -1 when map is NULL
 **/
 int my_hashmap_contains_from_data(my_hashmap *map, void const *data);
 
 /**
-    * Put a new row in HashMap
-    * @return 1 when ok. 0 when one arg is NULL
+* Put a new row in HashMap
+* @return 1 when ok. 0 when one arg is NULL
 **/
 int my_hashmap_put(my_hashmap **map, void *key, void *data);
 
 /**
-    * Replace the content of one row
-    * @return 1 when row is found and modified, 0 when error
+* Replace the content of one row
+* @return 1 when row is found and modified, 0 when error
 **/
 int my_hashmap_set(my_hashmap **map, void const *key, void *data);
 
 /**
-    * Count rows number in HashMap
+* Count rows number in HashMap
 **/
 int my_hashmap_count(my_hashmap *map);
 
 /**
-    * Transform the hashmap to a string array as format: "KEY=DATA"
-    *
-    * A segfault can appear if you put another type instead of char *
-    * @return char **
+* Transform the hashmap to a string array as format: "KEY=DATA"
+*
+* A segfault can appear if you put another type instead of char *
+* @return char **
 **/
 char **my_hashmap_to_string_array(my_hashmap *map);
 
 /**
-    * Transform the hashmap to an all type array as format: DATA
-    * @return void *
+* Transform the hashmap to an all type array as format: DATA
+* @return void *
 **/
 void **my_hashmap_convert(my_hashmap *map);
 
 /**
-    * Remove a row from key in HashMap
-    * @return int
+* Remove a row from key in HashMap
+* @return int
 **/
 int my_hashmap_remove(my_hashmap **map, void const *key);
 
