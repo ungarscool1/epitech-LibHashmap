96a222dc82be2c9e1f056f3c7443941c45ff041a
Léo Godard <leo.godard@epitech.eu>
2020-02-12 13:53:53 +0100

    [Fix] A malloced str can be now handled in get via key

--- src/hashmap_get.c
+++ src/hashmap_get.c
@@ -7,6 +7,25 @@
 
 #include "my_hashmap.h"
 
+static int get_strlen(char const *str)
+{
+    int len = 0;
+
+    for (; str[len] != '\0'; len++);
+    return len;
+}
+
+static int my_streq(char const *src, char const *str)
+{
+    if (get_strlen(src) != get_strlen(str))
+        return 0;
+    for (int i = 0; i < get_strlen(str); i++) {
+        if (src[i] != str[i])
+            return 0;
+    }
+    return 1;
+}
+
 void *my_hashmap_get(my_hashmap *map, void *key)
 {
     my_hashmap *node;
@@ -15,7 +34,7 @@ void *my_hashmap_get(my_hashmap *map, void *key)
         return NULL;
     node = map;
     while (node != NULL) {
-        if (node->key == key)
+        if (node->key == key || my_streq(node->key, key))
             return node->data;
         node = node->next;
     }
