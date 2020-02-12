904349e6f1bee49e4677a989416fbdee1a179b37
Léo Godard <leo.godard@epitech.eu>
2020-02-12 14:33:12 +0100

    [Fix] hashmap contains

--- src/my_hashmap_contains.c
+++ src/my_hashmap_contains.c
@@ -7,13 +7,34 @@
 
 #include "my_hashmap.h"
 
+static int get_strlen(char const *str)
+{
+    int len = 0;
+
+    for (; str[len] != '\0'; len++);
+    return len;
+}
+
+static int my_streq(char const *src, char const *str)
+{
+    if (get_strlen(src) != get_strlen(str))
+        return 0;
+    for (int i = 0; i < get_strlen(str); i++) {
+        if (src[i] != str[i])
+            return 0;
+    }
+    return 1;
+}
+
 int my_hashmap_contains(my_hashmap *map, void const *key)
 {
     my_hashmap *node;
 
+    if (map == NULL)
+        return 0;
     node = map;
     while (node != NULL) {
-        if (node->key == key)
+        if (node->key == key || my_streq(node->key, key))
             return 1;
         node = node->next;
     }
@@ -24,9 +45,11 @@ int my_hashmap_contains_from_data(my_hashmap *map, void const *data)
 {
     my_hashmap *node;
 
+    if (map == NULL)
+        return 0;
     node = map;
     while (node != NULL) {
-        if (node->data == data)
+        if (node->data == data || my_streq(node->data, data))
             return 1;
         node = node->next;
     }
