6c6815be730eea1b4ac2c67dcde1689e0b030c41
Léo Godard <leo.godard@epitech.eu>
2020-02-12 13:49:48 +0100

    [Fix] Fix remove crash

--- src/my_hashmap_set.c
+++ src/my_hashmap_set.c
@@ -7,6 +7,25 @@
 
 #include "my_hashmap.h"
 
+static int get_strlen(char const *str)
+{
+    int len = 0;
+
+    for (; str[len] != '\0'; len++);
+    return len;
+}
+
+static int my_streq(char const *src, char const *str)
+{
+    if (get_strlen(src) != get_strlen(str))
+        return 0;
+    for (int i = 0; i < get_strlen(str); i++) {
+        if (src[i] != str[i])
+            return 0;
+    }
+    return 1;
+}
+
 int my_hashmap_set(my_hashmap **map, void const *key, void *data)
 {
     my_hashmap *node;
@@ -15,7 +34,7 @@ int my_hashmap_set(my_hashmap **map, void const *key, void *data)
         return 0;
     node = (*map);
     while (node != NULL) {
-        if (node->key == key) {
+        if (node->key == key || my_streq(node->key, key)) {
             node->data = data;
             return 1;
         }
@@ -28,19 +47,25 @@ int my_hashmap_remove(my_hashmap **map, void const *key)
 {
     my_hashmap *next_node;
     my_hashmap *node;
-    my_hashmap *tmp;
+    my_hashmap *previous;
 
-    if (map == NULL || key == NULL || my_hashmap_contains((*map), key) != 1)
+    if (key == NULL || my_hashmap_contains((*map), key) != 1)
         return 0;
     node = (*map);
+    if (node->key == key || my_streq(node->key, key)) {
+        (*map) = node->next;
+        free(node);
+        return 1;
+    }
+    previous = node;
+    node = node->next;
     while (node != NULL) {
-        tmp = node->next;
-        if (tmp->key == key) {
-            next_node = tmp->next;
-            node->next = next_node;
-            free(tmp);
-            return 1;
+        if (node->key == key || my_streq(node->key, key)) {
+            previous->next = node->next;
+            free(node);
+        return 1;
         }
+        previous = node;
         node = node->next;
     }
     return 0;
